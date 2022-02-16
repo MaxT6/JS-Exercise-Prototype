5591d823461e63e2c2c0d1ebba8bb362dbd68ec3
Maximilian Thiel <maximilianthiel@maximilians-mbp.myfiosgateway.com>
2022-02-16 17:56:35 -0500

    created three object prototypes, with the last being an inheritence to the first. Defined the 4 principles of this.

--- index.js
+++ index.js
@@ -39,13 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  
-}
+function Person(name, age) {
+  this.name = name; 
+  this.age = age;
+  this.stomach = [];
+};
 
+Person.prototype.eat = function (someFood) {
+  if(this.stomach.length < 10); {
+    this.stomach.push(someFood);
+  } // else { return 'This stomach is full, and cannot eat.'}
+};
 
+Person.prototype.poop = function () {
+  this.stomach.length = [];
+  };
 
-
+Person.prototype.toString = function() {
+    return `${this.name} is ${this.age}.`
+};
 
 
 
@@ -63,9 +75,21 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function (gallons) {
+  this.tank += gallons;
+};
+Car.prototype.drive = function (distance) {
+  this.odometer += distance;
+  this.tank - (distance / this.milesPerGallon)
+};
+
 
 
 /*
@@ -75,10 +99,14 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
- 
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
-
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function() {
+  return `Playing with ${this.favoriteToy}.`
+};
 
 /* 
   TASK 4
